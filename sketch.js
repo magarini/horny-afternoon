ed6d0050216c384d2683540c8d44d833aaa279f5
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-11-30 22:42:00 +0200

    removed sentonia because of firefox bug

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 var canvas;
-let teapot;
-let laptop;
+let butterfly;
+let body;
 let sentonia;
 let mySound;
 let j=0;
+let glitterTexture;
 
 function preload() {
-    teapot = loadModel('butterfly.obj', true);
-    laptop = loadModel('Realistic_White_Female_Low_Poly.obj', true);
-  sentonia=loadImage('IMG_9227.jpg');
-    img = loadImage('glitter.jpg');
+    butterfly = loadModel('butterfly.obj', true);
+    body = loadModel('Realistic_White_Female_Low_Poly.obj', true);
+    // sentonia=loadImage('IMG_9227.jpg');
+    glitterTexture = loadImage('glitter.jpg');
 
     soundFormats('mp3', 'ogg');
     mySound = loadSound('horniness.mp3');
@@ -20,61 +21,71 @@ function preload() {
 function setup(){
     let canvas= createCanvas(windowWidth,windowHeight,WEBGL);
     canvas.mousePressed(canvasPressed);
-    textureMode(NORMAL);
 
   }
 
 
   function draw(){
+    textureMode(NORMAL);
+
     clear();
+
     push();
-    texture(sentonia);
+    noStroke();
+    // fill(222,111,111)
+    // texture(sentonia);
     sphere(windowWidth*2);
+    pop();
+
     ambientLight(80); // white light
 
-    pop();
     orbitControl();
-        pointLight(255,-mouseY,-mouseX);
+    pointLight(255,-mouseY,-mouseX);
     angleMode(DEGREES);
-    push();
+
     pointLight(255,mouseX,mouseY);
-  noStroke();
-   specularMaterial(255, 255, 255);
-   texture(img);
- scale(0.5);
-translate(-100+random(3),-100+random(3),250+random(3));
-// rotateZ(90);
-rotateX(90);
-    model(teapot);
+
+    push();
+    noStroke();
+    specularMaterial(255, 255, 255);
+    texture(glitterTexture);
+    scale(0.5);
+    translate(-100+random(3),-100+random(3),250+random(3));
+    // rotateZ(90);
+    rotateX(90);
+    model(butterfly);
    pop();
+
    push();
    //pointLight(255,mouseX,mouseY);
    rotateX(180);
 
  noStroke();
   specularMaterial(255, 255, 255);
-   texture(img);
+   texture(glitterTexture);
 //shininess(10);
 scale(0.5);
-texture(img);
+texture(glitterTexture);
 translate(-100+random(3),100+random(3),250+random(20));
 rotateZ(90);
 rotateX(90);
-   model(teapot);
+   model(butterfly);
   pop();
+
+
   push();
   //pointLight(255,mouseX,mouseY);
 // rotateX(frameCount * 0.1);
 // rotateX(200);
 noStroke();
  specularMaterial(255, 255, 255);
-   texture(img);
+   texture(glitterTexture);
 //shininess(10);
 scale(0.5);
 translate(100+random(10),100+random(3),200+random(3));
 rotateZ(90);
 // rotateX(90);
-  model(teapot);
+  model(butterfly);
  pop();
  push();
  //pointLight(255,mouseX,mouseY);
@@ -82,13 +93,13 @@ rotateZ(90);
 // rotateX(200);
 noStroke();
 specularMaterial(255, 255, 255);
-  texture(img);
+  texture(glitterTexture);
 //shininess(10);
 scale(0.5);
 translate(100+random(3),200+random(10),200+random(3));
 rotateZ(90);
 rotateX(90);
- model(teapot);
+ model(butterfly);
 pop();
 push();
 //pointLight(255,mouseX,mouseY);
@@ -100,24 +111,25 @@ rotateX(180);
 
 noStroke();
 specularMaterial(255, 204, 204);
-//  texture(img);
+//  texture(glitterTexture);
 //shininess(10);
 // scale(3);
 // translate(-100,-200,-100);
 scale(5);
-model(laptop);
+model(body);
 pop();
 
 // for (i=0;i<100;i++){
 directionalLight(255,100,220,0);
 // }
 
-
+push();
 translate(0,0,0);
 noStroke();
  fill(255,99,71);
 
 sphere(random(100));
+pop();
   }
 
 
